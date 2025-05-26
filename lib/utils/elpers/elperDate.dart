75825437c1bfe4ac7d612f3477c12ae96d04f0a5
marien <mmanima322@gmail.com>
2025-05-26 13:18:12 -0700

    configuration de quelques configuration et adaptation de la date

--- lib/utils/elpers/elperDate.dart
+++ lib/utils/elpers/elperDate.dart
@@ -1,5 +1,5 @@
 import 'package:intl/intl.dart';
 
 String dateDuJour() {
-  return DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
+  return DateFormat('dd-MM-yyyy HH:mm:ss').format(DateTime.now());
 }
